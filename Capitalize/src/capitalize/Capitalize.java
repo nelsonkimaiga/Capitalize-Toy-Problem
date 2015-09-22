a9f06c488b169a22430858510951ae6da50063fa
Nelson Kimaiga <nelsonkimaiga@aol.com>
2015-09-22 11:14:53 +0300

    final build and working

--- Capitalize/src/capitalize/Capitalize.java
+++ Capitalize/src/capitalize/Capitalize.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package capitalize;
-
+import java.util.Scanner;
 /**
  *
  * @author Nelson
@@ -14,8 +14,24 @@ public class Capitalize {
     /**
      * @param args the command line arguments
      */
+   public static String capital(String str){
+    String[] newStr = str.split(" ");
+    StringBuffer strbuff = new StringBuffer();
+
+    for(int i = 0; i < newStr.length; i++){
+      strbuff.append(Character.toUpperCase(newStr[i].charAt(0)))
+      .append(newStr[i].substring(1)).append(" ");
+    }
+
+    return strbuff.toString().trim();
+  }   
+    
     public static void main(String[] args) {
         // TODO code application logic here
+            Scanner s = new Scanner(System.in);
+    System.out.println("enter string: ");
+    System.out.println(capital(s.nextLine()));
     }
     
 }
+
